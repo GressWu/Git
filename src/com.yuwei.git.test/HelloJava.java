e78d56efb155fc0795e576add2b747b16f241471
ywwu1 <ywwu1123>
2020-10-11 18:32:58 +0800

    新增类

---
+++ src/com.yuwei.git.test/HelloJava.java
@@ -0,0 +1,9 @@
+package com.yuwei.git.test;
+
+public class HelloJava {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            System.out.println(i);
+        }
+    }
+}
