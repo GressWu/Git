39a4288f98141a137fbfc7177c3fd1f9d9a72001
ywwu1 <ywwu1123>
2020-10-11 17:59:34 +0800

    直接Commit代码

--- src/com.yuwei.git.test/HelloWorld.java
+++ src/com.yuwei.git.test/HelloWorld.java
@@ -3,5 +3,6 @@ package com.yuwei.git.test;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("天朗气清惠风和畅");
     }
 }
